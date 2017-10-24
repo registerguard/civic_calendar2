053e159ff003c4ccfe63b1b3cc9dbfba18be475d
J. Heasly <jheasly@earthlink.net>
2017-10-23 18:18:01 -0700

    Start of town-based iCal feed.

---
+++ feeds/ical.py
@@ -0,0 +1,8 @@
+from schedule.feeds.ical import ICalendarFeed
+
+# A starting point:
+# https://github.com/llazzaro/django-scheduler/blob/develop/schedule/feeds/__init__.py#L49
+
+class TownICalendar(ICalendarFeed):
+    def items(self):
+        pass
