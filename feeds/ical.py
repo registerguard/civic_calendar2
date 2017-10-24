77cc2351b81174320ed6c2b1e64e4f08ee4b2983
J. Heasly <jheasly@earthlink.net>
2017-10-23 18:27:58 -0700

    A little more for the iCal.

--- feeds/ical.py
+++ feeds/ical.py
@@ -1,8 +1,10 @@
 from schedule.feeds.ical import ICalendarFeed
+from schedule.models import Event
 
 # A starting point:
 # https://github.com/llazzaro/django-scheduler/blob/develop/schedule/feeds/__init__.py#L49
 
 class TownICalendar(ICalendarFeed):
     def items(self):
-        pass
+        return Event.objects.filter(entity__jurisdiction__slug=slug).\
+            order_by('-start')
